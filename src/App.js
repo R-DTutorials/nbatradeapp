5fac4cff6d61026eb440bd201c0cd7e1a8870cd7
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2020-05-21 01:19:43 -0400

    add player api

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
     }
 
     componentDidMount() {
-        const teamEndpoint = 'http://data.nba.net/data/10s/prod/v1/2019/teams.json';
-        fetch(teamEndpoint)
+        const teamsEndpoint = 'http://data.nba.net/data/10s/prod/v1/2019/teams.json';
+        fetch(teamsEndpoint)
             .then((res) => res.json())
             .then((data) => {
                 const teams = data.league.standard.filter((team) => team.isNBAFranchise);
@@ -25,12 +25,41 @@ class App extends Component {
             });
     }
 
+    getPlayersOnTeam(team) {
+        const players = [];
+        const teamEndpoint = `http://data.nba.net/data/10s/prod/v1/2019/teams/${team}/roster.json`;
+        fetch(teamEndpoint)
+            .then((res) => res.json())
+            .then((data) => {
+                players.push(data.league.standard.players);
+            });
+
+        // if (players.length) {
+        //     players.map(({ personId }) => {
+        //         const playerEndpoint = `http://data.nba.net/data/10s/prod/v1/2019/players/${personId}_profile.json`;
+        //         fetch(playerEndpoint)
+        //             .then((res) => res.json())
+        //             .then((data) => {
+        //                 players.push(data);
+        //             })
+        //     });
+        // }
+        //
+        // this.setState({
+        //     players: players
+        // });
+    }
+
     setTeam(e, team) {
         this.setState({ [team]: e.target.value });
     }
 
     render() {
-        console.log(this.state)
+        const { teams, players } = this.state;
+        if (teams.length) {
+            this.getPlayersOnTeam(teams[0].urlName);
+            console.log("players", players);
+        }
         return (
             <div className="App">
                 <header className="App-header">NBA Trade App</header>
