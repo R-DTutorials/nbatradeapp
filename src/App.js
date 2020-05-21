94a08e5de41547425c4965b2d0d9ced96706c879
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2020-05-21 12:04:55 -0400

    get players

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React , { Component } from 'react';
+import PlayerCard from "./PlayerCard";
 import './App.css';
 
 const getTeamsDropdown = (teams) => {
-    return teams && teams.map(team => <option value={team.fullName}>{team.fullName}</option>)
+    return teams && teams.map(team => <option key={team.teamId} value={team.urlName}>{team.fullName}</option>)
 };
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
         this.state = {
             teams: [],
             team1: '',
-            team2: ''
+            team2: '',
         }
     }
 
@@ -23,52 +24,73 @@ class App extends Component {
                 const teams = data.league.standard.filter((team) => team.isNBAFranchise);
                 this.setState({ teams });
             });
+        this.getPlayersOnTeam('team1players', 'hawks');
+        this.getPlayersOnTeam('team2players', 'hawks');
     }
 
-    getPlayersOnTeam(team) {
-        const players = [];
-        const teamEndpoint = `http://data.nba.net/data/10s/prod/v1/2019/teams/${team}/roster.json`;
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(nextState)
+        if (nextState.team1 !== this.state.team1) {
+            this.getPlayersOnTeam('team1players', nextState.team1);
+        }
+        if (nextState.team2 !== this.state.team2) {
+            this.getPlayersOnTeam('team2players', nextState.team2);
+        }
+        return true;
+    }
+
+    getPlayersOnTeam(choice, team) {
+        const teamEndpoint = `http://data.nba.net/json/cms/noseason/team/${team}/roster.json`;
         fetch(teamEndpoint)
             .then((res) => res.json())
             .then((data) => {
-                players.push(data.league.standard.players);
+                console.log("i", data)
+                this.setState({
+                    [choice]: data.sports_content.roster.players.player,
+                });
             });
-
-        // if (players.length) {
-        //     players.map(({ personId }) => {
-        //         const playerEndpoint = `http://data.nba.net/data/10s/prod/v1/2019/players/${personId}_profile.json`;
-        //         fetch(playerEndpoint)
-        //             .then((res) => res.json())
-        //             .then((data) => {
-        //                 players.push(data);
-        //             })
-        //     });
-        // }
-        //
-        // this.setState({
-        //     players: players
-        // });
     }
 
     setTeam(e, team) {
         this.setState({ [team]: e.target.value });
     }
 
+    renderPlayers(players) {
+        return (
+            <div className="card-container">
+                {players.map((player) => {
+                    const { first_name, last_name, jersey_number } = player;
+                    return (
+                        <div className="player-card">
+                            <span className="player-name">{`${first_name} ${last_name}`}</span>
+                            <span className="player-jersey">{`#${jersey_number}`}</span>
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
+
     render() {
-        const { teams, players } = this.state;
-        if (teams.length) {
-            this.getPlayersOnTeam(teams[0].urlName);
-            console.log("players", players);
+        const { teams, team1players, team2players } = this.state;
+        let playerCards1, playerCards2 = null;
+        if (team1players && team2players) {
+            playerCards1 = this.renderPlayers(team1players);
+            playerCards2 = this.renderPlayers(team2players);
         }
         return (
             <div className="App">
                 <header className="App-header">NBA Trade App</header>
-                <select name="team1" id="team1" onChange={ (e) => this.setTeam(e, 'team1') } value={this.state.team1}>
-                    { getTeamsDropdown(this.state.teams.filter(team => team.fullName !== this.state.team2)) }
-                </select>
-                <select name="team2" id="team2" onChange={ (e) => this.setTeam(e, 'team2') } value={this.state.team2}>
-                    { getTeamsDropdown(this.state.teams.filter(team => team.fullName !== this.state.team1)) }
-                </select>
+                <div className="select-container">
+                    <select name="team1" id="team1" onChange={ (e) => this.setTeam(e, 'team1') } value={this.state.team1}>
+                        { getTeamsDropdown(teams.filter(team => team.urlName !== this.state.team2)) }
+                    </select>
+                    <select name="team2" id="team2" onChange={ (e) => this.setTeam(e, 'team2') } value={this.state.team2}>
+                        { getTeamsDropdown(teams.filter(team => team.urlName !== this.state.team1)) }
+                    </select>
+                </div>
+               {playerCards1}
+               {playerCards2}
             </div>
         )
     }
