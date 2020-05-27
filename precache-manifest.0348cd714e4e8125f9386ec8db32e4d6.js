37f4884f8d037dd17ff0770311b62a80205d4c9c
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2020-05-26 23:37:21 -0400

    Updates

---
+++ precache-manifest.0348cd714e4e8125f9386ec8db32e4d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8339a3784ce74feed3195ba94c35316",
+    "url": "/nbatradeapp/index.html"
+  },
+  {
+    "revision": "338b02a1dbecf917b660",
+    "url": "/nbatradeapp/static/css/main.edfc1f6f.chunk.css"
+  },
+  {
+    "revision": "975f171acb3af9356656",
+    "url": "/nbatradeapp/static/js/2.8f3d162d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nbatradeapp/static/js/2.8f3d162d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338b02a1dbecf917b660",
+    "url": "/nbatradeapp/static/js/main.4f9e7b27.chunk.js"
+  },
+  {
+    "revision": "58f477feb93181954427",
+    "url": "/nbatradeapp/static/js/runtime-main.0c18265f.js"
+  }
+]);
