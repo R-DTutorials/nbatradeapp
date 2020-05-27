8e3dd280653a621294ccb40aac68fa14121ff1e7
Raj Ashish Desai <rajdesai94@gmail.com>
2020-05-27 15:16:26 -0400

    wip - roster steps

--- src/App.test.js
+++ src/App.test.js
@@ -65,7 +65,7 @@ export default SelectTeam;
 import SelectTeam from "./components/SelectTeam";
 
 // select default selected teams in the state (team1, team2)
-this.state ={
+this.state = {
   team1: 'warriors',
   team2: 'clippers',
   allTeams: data,
@@ -73,12 +73,12 @@ this.state ={
 
 // pass prop team and show page to view default teams warriors and clippers
 <SelectTeam
-id="team1"
-availableTeams={allTeams}
-team={team1} // prop that is a string that is the team currently selected in the dropdown
+  id="team1"
+  availableTeams={allTeams}
+  team={team1} // prop that is a string that is the team currently selected in the dropdown
 />
 
-// declare value in select 
+// declare value in select
 return (
   <select name={id} id={id} value={team}>
       {
@@ -95,12 +95,12 @@ setTeam(e, team) {
   this.setState({ [team]: e.target.value });
 }
 
-// pass prop setTeam function  
+// pass prop setTeam function
 <SelectTeam
-id="team1"
-availableTeams={allTeams}
-setTeam={(e) => this.setTeam(e, 'team1')} // when clicking on team in dropdown this func sets the team value in the state under 'team1' porperty or 'team2' property
-team={team1} // prop that is a string that is the team currently selected in the dropdown
+  id="team1"
+  availableTeams={allTeams}
+  setTeam={(e) => this.setTeam(e, 'team1')} // when clicking on team in dropdown this func sets the team value in the state under 'team1' porperty or 'team2' property
+  team={team1} // prop that is a string that is the team currently selected in the dropdown
 />
 
 // add onchange prop
@@ -122,4 +122,101 @@ return (
   team={team1} // prop that is a string that is the team currently selected in the dropdown
 />
 
+// We need 2 new roster components to show the players on both teams, pass the team
+<Roster
+  franchise={this.state.allTeams.filter((franchise) => franchise.team.team_code === team1)} // filters all teams to only pass the selected teams data in order to render the player cards for that team
+/>
 
+// create Roster Child component (Roster.js)
+import React from 'react';
+
+const Roster = ({ franchise }) => (
+    <div className="card-container">
+        {franchise[0].players.map((player) => { // render playercards for selected franchise
+            return (
+                <PlayerCard
+                    key={player.person_id}
+                    player={player}
+                    teamAbbreviation={franchise[0].team.team_abbrev}
+                />
+            );
+        })}
+    </div>
+);
+
+export default Roster;
+
+// create Player Card component (PlayerCard.js)
+import React from 'react';
+
+const experience = {
+	0: 'Rookie',
+	1: '1 year'
+};
+
+const PlayerCard = (props) => {
+	const { player } = props;
+	const {
+		first_name,
+		last_name,
+		jersey_number,
+		height_ft,
+		height_in,
+		position_full,
+		weight_lbs,
+		years_pro,
+	} = player; // player object stats
+
+	return (
+		<div className="player-card">
+      <div className="player-name">{`${first_name} ${last_name}`}</div>
+			<div className="player-specs">{`${teamAbbreviation} | #${jersey_number} | ${position_full.toUpperCase()}`}</div>
+			<div className="player-height">{`Height: ${height_ft}"${height_in}`}</div>
+			<div className="player-weight">{`Weight: ${weight_lbs} lbs`}</div>
+			<div className="player-exp">{`Experience: ${experience[years_pro] || years_pro + ' years'}`}</div>
+		</div>
+	);
+}
+
+export default PlayerCard;
+
+// set empty values in the state that we will populate with the players select for trade (left, right)
+this.state = {
+  left: '',
+  right: '',
+  team1: 'warriors',
+  team2: 'clippers',
+  allTeams: data,
+}
+
+// Create function to select a player and pass it through to PlayerCard for 'onClick'
+selectPlayer(player, side) {
+  const currentPlayerId = this.state[side].person_id;
+  this.setState({
+      [side]: currentPlayerId !== player.person_id ? player : '', // add ability to unselect a player also
+  });
+}
+
+// Pass the appropriate selected player to each roster so we can style it and show which player might have been selected
+// Also pass the function selectPlayer so we can set a new player
+<Roster
+  franchise={this.state.allTeams.filter((franchise) => franchise.team.team_code === team1)} // filters all teams to only pass the selected teams data in order to render the player cards for that team
+  side={this.state.right} // this prop passes down the player object info from the corresponding side to PlayerCard to render certain player
+  selectPlayer={(player) => this.selectPlayer(player,"right")} // func that selects player (sets player object in state) which is called when user clicks on the PlayerCard
+/>
+
+// Pass chosen player and func to select player through to PlayerCard
+const Roster = ({ franchise, side, ...otherProps }) => (
+  <div className="card-container">
+      {franchise[0].players.map((player) => { // render playercards for selected franchise
+          return (
+              <PlayerCard
+                  key={player.person_id}
+                  player={player}
+                  selected={side.person_id === player.person_id ? 'selected' : ''}
+                  { ...otherProps }
+              />
+          );
+      })}
+  </div>
+);
